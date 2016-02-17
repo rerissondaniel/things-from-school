49e2e43786d0334d8518caca61394281bb4c2c60
Rerisson Daniel <rerissondaniel@gmail.com>
2016-02-16 23:13:06 -0300

    added import.sql

---
+++ web-programming/PrimeiraAvaliacao4bimProgWeb/src/import.sql
@@ -0,0 +1,13 @@
+insert into tb_usuario(id, nome, matricula, data_nascimento) values (0, "Matheus Coutinha", "20121004001", "01/04/1995");
+insert into tb_usuario(id, nome, matricula, data_nascimento) values (0, "Juan Barros", "20121004007", "12/05/1996");
+insert into tb_usuario(id, nome, matricula, data_nascimento) values (0, "Gustavo Ribeiro", "20121004002", "13/12/1313");
+insert into tb_usuario(id, nome, matricula, data_nascimento) values (0, "Juliana Pereira", "20121004003", "03/07/1996");
+insert into tb_usuario(id, nome, matricula, data_nascimento) values (0, "Myllena Alexandre", "20121004005", "09/05/1999");
+insert into tb_usuario(id, nome, matricula, data_nascimento) values (0, "Rerisson Daniel", "20121004004", "06/09/1996");
+insert into tb_usuario(id, nome, matricula, data_nascimento) values (0, "Rayane Oliveira", "20121004006", "05/03/1997");
+insert into tb_usuario(id, nome, matricula, data_nascimento) values (0, "Lorrany Andrade", "20121004008", "08/01/1997");
+insert into tb_usuario(id, nome, matricula, data_nascimento) values (0, "Airton Silva", "20121004009", "12/12/1997");
+insert into tb_usuario(id, nome, matricula, data_nascimento) values (0, "Gabriella Araújo", "20121004010", "14/03/1990");
+insert into tb_usuario(id, nome, matricula, data_nascimento) values (0, "Matheus Vasconscelos", "20121004011", "01/07/1990");
+insert into tb_usuario(id, nome, matricula, data_nascimento) values (0, "Matheus Cunha", "20121004012", "02/09/1990");
+insert into tb_usuario(id, nome, matricula, data_nascimento) values (0, "Leonardo Silva", "20121004013", "01/06/1990");
